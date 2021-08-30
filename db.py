5bacba0840ab69a31864b59ca32c99fafae96bfb
Jewel James <flakyhermit@protonmail.ch>
2021-08-30 16:19:15 +0530

    WIP: Made foreign key work + functions

--- db.py
+++ db.py
@@ -16,26 +16,60 @@ class Db:
 
         self.cur.execute("""CREATE TABLE IF NOT EXISTS clippings (
         clip_id INTEGER PRIMARY KEY,
-        book_id INTEGER,
-        FOREIGN KEY (book_id)
-            REFERENCES books (book_id),
         location INTEGER,
         page INTEGER,
         timestamp INTEGER,
         highlight TEXT,
-        note TEXT);""")
+        note TEXT,
+        book_id INTEGER,
+        FOREIGN KEY (book_id) REFERENCES books (book_id));""")
         self.conn.commit()
 
     def close(self):
         self.conn.close()
 
-    def insert_book(self, book):
-        self.cur.execute("INSERT books (title, author) VALUES (:title, :author)", {title: book.title, author: book.author})
+    def get_book_id (self, title, author):
+        self.cur.execute("""SELECT book_id, author, title
+                            FROM books
+                            WHERE title=? AND author=?""",
+                         (title, author))
+        _id = self.cur.fetchone()
+        return _id
+
+    def insert_book(self, title, author):
+        self.cur.execute("""SELECT book_id, author, title
+                            FROM books
+                            WHERE title=? AND author=?""",
+                         (title, author))
+        result = self.cur.fetchone()
+        if result:
+            raise Exception('Book already exists with id: %s' % result[0])
+        self.cur.execute("""INSERT INTO books (title, author)
+                            VALUES (?, ?)""", (title, author))
+        self.conn.commit()
+        _id = self.cur.execute("""SELECT book_id FROM books WHERE title=? AND author=?""",
+                                  (title, author)).fetchone()
+        return _id
 
     def insert_clip(self, clip):
         """Insert a clip into database."""
-        self.cur.exeutemany("""INSERT into clippings(text)
-        VALUES (?,?,?)""", clip)
+        self.cur.exeute("""INSERT into
+                           clippings(location, page, timestamp, highlight, note, book_id)
+                           VALUES (?,?,?,?,?,?)""", clip)
+        self.conn.commit()
 
-    def check_if_exists(clip):
-        """Check if clip already exists in db."""
+    def insert_clips(self, clips):
+        """Insert many clips"""
+        self.cur.exeutemany("""INSERT into
+                           clippings(location, page, timestamp, highlight, note, book_id)
+                           VALUES (?,?,?,?,?,?)""", clips)
+        self.conn.commit()
+
+    # Functions to print the database tables
+    def print_clips(self):
+        results = self.cur.execute("""SELECT * FROM clippings""").fetchall()
+        print(results)
+
+    def print_books(self):
+        results = self.cur.execute("""SELECT * FROM books""").fetchall()
+        print(results)
